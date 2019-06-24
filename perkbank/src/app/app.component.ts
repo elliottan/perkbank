0c6749c422e3c5642cc6b50459b18312c5cca596
Elliot <>
2019-06-25 00:06:21 +0800

    add basic login page and login button press handler

--- perkbank/src/app/app.component.ts
+++ perkbank/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'perkbank';
+
+  login(username, password) {
+  	console.log(username)
+  	console.log(password)
+  }
 }
