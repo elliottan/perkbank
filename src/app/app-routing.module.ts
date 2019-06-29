385762f00628bfb94cf72eec5d8c1eec130821e7
Elliot <>
2019-06-29 12:39:01 +0800

    created dashboard, login, login-select components
    - updated app routing
    - added auth guard guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginSelectComponent } from './login-select/login-select.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path      : '',
+        pathMatch : 'full',
+        redirectTo: 'login',
+    },
+    {
+        path      : 'dashboard',
+        component : DashboardComponent,
+        canActivate: [AuthGuard]
+ 
+    },
+    {
+        path      : 'login-select',
+        component : LoginSelectComponent
+    },
+    {
+        path      : 'login',
+        component : LoginComponent
+    }
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AuthGuard } from './auth.guard';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginSelectComponent } from './login-select/login-select.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginSelectComponent
+    LoginSelectComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
