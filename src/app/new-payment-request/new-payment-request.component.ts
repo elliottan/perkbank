5d2a5dca8cfff04443887910eaf39b365ac2afd8
Elliot <>
2019-07-02 04:32:55 +0800

    new payment request component skeletons

---
+++ src/app/new-payment-request/new-payment-request.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-payment-request',
+  templateUrl: './new-payment-request.component.html',
+  styleUrls: ['./new-payment-request.component.css']
+})
+export class NewPaymentRequestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
